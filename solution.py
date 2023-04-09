529433e024f5d20f6d47b95f028a0cf92a3d351c
Alexei Borovoi <81176384+AlexDk78@users.noreply.github.com>
2023-04-09 11:41:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,8 +4,14 @@ import numpy as np
 
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(X, Y) -> bool:
+    from scipy.stats import ttest_ind
+    alpha = 0.07
+    n1, n2 = len(X), len(Y)
+
+    t_stat, p_value = ttest_ind(X, Y, equal_var=True)
+    
+    if p_value/2 < alpha and t_stat > 0:
+        return True
+    else:
+        return False
