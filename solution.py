c65d92655c2da70a733a047c9760ed2c84a1e1c3
Alexei Borovoi <81176384+AlexDk78@users.noreply.github.com>
2023-04-10 19:38:22 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,17 +1,8 @@
 import pandas as pd
 import numpy as np
-
+import scipy.stats as stats
 
 chat_id = 897113152 # Ваш chat ID, не меняйте название переменной
 
-def solution(X, Y) -> bool:
-    from scipy.stats import ttest_ind
-    alpha = 0.07
-    n1, n2 = len(X), len(Y)
-
-    t_stat, p_value = ttest_ind(X, Y, equal_var=True)
-    
-    if p_value/2 < alpha and t_stat > 0:
-        return True
-    else:
-        return False
+def solution(x, y) -> bool:
+    return stats.ttest_ind(x, y)[1] < 0.07
