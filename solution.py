0eb07bed54bb8015c3b153284bdac5f0426b873f
Alexei Borovoi <81176384+AlexDk78@users.noreply.github.com>
2023-04-10 19:56:23 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,30 @@
 import pandas as pd
 import numpy as np
-import scipy.stats as stats
+from scipy import stats
+import math
 
 chat_id = 897113152 # Ваш chat ID, не меняйте название переменной
 
-def solution(x, y) -> bool:
-    return stats.ttest_ind(x, y)[1] < 0.07
+def solution(x_success: int, 
+             x_cnt: int, 
+             y_success: int, 
+             y_cnt: int) -> bool:
+
+    alpha=0.07
+    
+    p1 = x_success / x_cnt
+    p2 = y_success / y_cnt
+    p_comb = (x_success + y_success) / (x_cnt + y_cnt)
+    difference = p1 - p2
+ 
+    z_value = difference / math.sqrt(
+        p_comb * (1 - p_comb) * (1 / x_success + 1 / y_success)
+    ) 
+    distr = stats.norm(0, 1)  
+    
+    p_value = (1 - distr.cdf(abs(z_value))) * 2 
+    
+    if (p_value < alpha):
+      return True
+    else: 
+      return False
