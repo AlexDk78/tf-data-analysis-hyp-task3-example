bb4cd8c1f45a3d8199508aff13bfdb1fd031ca78
Alexei Borovoi <81176384+AlexDk78@users.noreply.github.com>
2023-04-12 21:50:16 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,30 +1,14 @@
 import pandas as pd
 import numpy as np
-from scipy import stats
-import math
+import scipy.stats as sps
 
 chat_id = 897113152 # Ваш chat ID, не меняйте название переменной
 
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
-             y_cnt: int) -> bool:
+def solution(x: np.array, y: np.array) -> bool: 
+    p_value = sps.permutation_test((x, y), lambda x, y, axis: np.mean(x, axis=axis) - np.mean(y, axis=axis), 
+                 vectorized=True, 
+                 n_resamples=5000,
+                 alternative='greater').pvalue 
+    alpha = 0.07
+    return p_value < alpha
 
-    alpha=0.07
-    
-    p1 = x_success / x_cnt
-    p2 = y_success / y_cnt
-    p_comb = (x_success + y_success) / (x_cnt + y_cnt)
-    difference = p1 - p2
- 
-    z_value = difference / math.sqrt(
-        p_comb * (1 - p_comb) * (1 / x_success + 1 / y_success)
-    ) 
-    distr = stats.norm(0, 1)  
-    
-    p_value = (1 - distr.cdf(abs(z_value))) * 2 
-    
-    if (p_value < alpha):
-      return True
-    else: 
-      return False
